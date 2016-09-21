f07f15c4b046f5929cd022a0a81ca98f8d211f40
Joseph <jvg_0119@yahoo.com>
2016-09-21 12:08:06 -0700

    resolving conflict on setup_mail.rb

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
-if Rails.env.development? || Rails.env.production?  # for both dev & production env
+
+if Rails.env.development? || Rails.env.production?  
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     address:        'smtp.sparkpostmail.com',
@@ -7,22 +7,7 @@ if Rails.env.development? || Rails.env.production?  # for both dev & production 
     authentication: :plain,
     user_name:      ENV['SPARKPOST_SMTP_USERNAME'],
     password:       ENV['SPARKPOST_SMTP_PASSWORD'],
-=======
-#if Rails.env.development?
-if Rails.env.development? || Rails.env.production?  ## for production
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
-    address:        'smtp.sendgrid.net',
-    port:           '587',
-    authentication: :plain,
-    user_name:      ENV['SENDGRID_USERNAME'],
-    password:       ENV['SENDGRID_PASSWORD'],
->>>>>>> cadabdea8f2cbfb9f1a281c8a5e05cecc3446caf
     domain:         'heroku.com',
     enable_starttls_auto: true
   }
 end
-<<<<<<< HEAD
-
-=======
->>>>>>> cadabdea8f2cbfb9f1a281c8a5e05cecc3446caf
